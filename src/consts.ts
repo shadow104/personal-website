f62ad0b540c42d65a9e114b34180eed4df96782a
shadow104 <hello@ravindu.cc>
2025-04-06 19:43:19 +0200

    Update site details and technology stack
    - Changed site title and author information in consts.ts
    - Updated contact information for social links
    - Modified technology stack in index.astro with new tools and platforms
    - Revised section headings and comments for clarity

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
+  TITLE: "Ravindu.cc",
   DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  AUTHOR: "Ravindu Dabarera",
 }
 
 // Work Page
@@ -45,10 +45,10 @@ export const LINKS: Links = [
     TEXT: "Blog", 
     HREF: "/blog", 
   },
-  { 
-    TEXT: "Projects", 
-    HREF: "/projects", 
-  },
+  // { 
+  //   TEXT: "Projects", 
+  //   HREF: "/projects", 
+  // },
 ]
 
 // Socials
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "hello@ravindu.cc",
+    HREF: "mailto:hello@ravindu.cc",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "shadow104",
+    HREF: "https://github.com/shadow104"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "ravinduchamara",
+    HREF: "https://www.linkedin.com/in/ravinduchamara/",
   },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+  // { 
+  //   NAME: "Twitter",
+  //   ICON: "twitter-x",
+  //   TEXT: "markhorn_dev",
+  //   HREF: "https://twitter.com/markhorn_dev",
+  // },
 ]
 
